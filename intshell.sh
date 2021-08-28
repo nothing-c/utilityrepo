c80c11fe0421b796f407f5823738c05a137b79f1
nothing-c <55337124+nothing-c@users.noreply.github.com>
2021-08-28 15:20:01 -0500

    Add files via upload

---
+++ intshell.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#$1 = first arg :)
+#use: ./(thisfile).sh filename.sh
+
+chmod u+x $1
+
+return 0
---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+day=$(date)
+uptime=$(uptime)
+echo "Hello, master"
+
+echo "The date is $day" 
+
+echo "The box has been up for $uptime"
+
+
+
