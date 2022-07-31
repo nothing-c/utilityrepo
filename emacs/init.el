1bbeffe2265360e5a0dfe6a6e44aec2458b638de
nothing-c <55337124+nothing-c@users.noreply.github.com>
2022-07-31 12:58:44 -0500

    Added ido-mode enable

--- emacs/init.el
+++ emacs/init.el
@@ -24,6 +24,11 @@
 (put 'dired-find-alternate-file 'disabled nil)
 ;;fundamental mode sucks
 (setq-default major-mode 'text-mode)
+;; use ido-mode
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(setq ido-create-new-buffer 'always)
+(ido-mode 1)
 
 ;;THE FUNCTION ZONE WOOOO
 (defun esh-buf ()
