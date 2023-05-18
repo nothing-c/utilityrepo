95046938719d686e23aa0d049db4027e3d758f88
nothing-c <55337124+nothing-c@users.noreply.github.com>
2023-05-18 12:49:45 -0500

    Fix missing paren

--- emacs/google.el
+++ emacs/google.el
@@ -2,7 +2,7 @@
 ;; Yeah, it's not difficult, shut up
 
 (defun google ()
-  "Quick google function (you will need to substitute spaces with '+'"
+  "Quick google function (you will need to substitute spaces with '+')"
   (interactive)
   (let ((search (read-string "Google:")))
     (eww (concat "https://www.google.com/search?&q=" search))))
