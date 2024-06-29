41e35503ed9c3717768cd56b28fe21eba176d972
nothing-c <55337124+nothing-c@users.noreply.github.com>
2024-06-29 16:03:25 -0500

    Add GC optimization to init file

--- emacs/init.el
+++ emacs/init.el
@@ -150,3 +150,21 @@
 
 ;;hide backups
 (setq backup-directory-alist `(("." . "~/autosaves")))
+
+;; optimize gc from https://akrl.sdf.org/#orgc15a10d
+(defmacro k-time (&rest body)
+  "Measure and return the time it takes evaluating BODY."
+  `(let ((time (current-time)))
+     ,@body
+     (float-time (time-since time))))
+
+;; Set garbage collection threshold to 1GB.
+(setq gc-cons-threshold #x40000000)
+
+;; When idle for 15sec run the GC no matter what.
+(defvar k-gc-timer
+  (run-with-idle-timer 15 t
+                       (lambda ()
+                         (message "Garbage Collector has run for %.06fsec"
+                                  (k-time (garbage-collect))))))
+
